746abb2169ca891b613bda765d4de6f039dfb30e
Daniel Sales <danisales.es@gmail.com>
2024-03-27 23:05:21 +0100

    feat: path alias

---
+++ path-alias/import-subpaths/src/index.ts
@@ -0,0 +1,4 @@
+import { add, subtract } from "#modules/module";
+
+console.log(add(1, 2));
+console.log(subtract(3, 1));
--- path-alias/tsc-alias/src/index.ts
+++ path-alias/tsc-alias/src/index.ts
@@ -1,5 +1,3 @@
-import { add } from "#lib/utils";
 import { multiply } from "#modules/module";
 
-console.log(add(1, 2));
 console.log(multiply(2, 3));
